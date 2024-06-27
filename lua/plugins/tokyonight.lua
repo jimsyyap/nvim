3bde1a5cf2e5a731b23a54687322a3bed655d65f
jimmydesu <jimsyyap@gmail.com>
2024-06-27 18:57:08 +1000

    tokyonight gone?

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,11 +1,12 @@
 return {
 	"folke/tokyonight.nvim",
 	name = "tokyonight",
-	opts = {
-		transparent = true,
-		styles = {
-			sidebars = "transparent",
-			floats = "transparent",
-		},
-	},
+	Lazy = true,
+	-- opts = {
+	-- 	transparent = true,
+	-- 	styles = {
+	-- 		sidebars = "transparent",
+	-- 		floats = "transparent",
+	-- 	},
+	-- },
 }
